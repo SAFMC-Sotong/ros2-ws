3a6fbaf2cc45b7b609a2cc8327e3d88d68062782
Shoon Kit <50212147+limshoonkit@users.noreply.github.com>
2025-03-16 23:10:00 +0800

    Updated bash files

---
+++ csi.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+source /home/nvidia/workspaces/ros2/install/setup.bash 
+
+gst-launch-1.0 nvarguscamerasrc sensor-id=0 ! 'video/x-raw(memory:NVMM), width=320, height=240, format=NV12, framerate=30/1' ! nvvidconv flip-method=2 ! 'video/x-raw, format=I420' ! jpegenc quality=60 ! udpsink host=10.42.0.180 port=5000 sync=false &> video_viewer_out.log &
+
+ros2 launch stream_data stream_data.launch.py
